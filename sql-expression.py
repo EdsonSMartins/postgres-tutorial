5d370ab9637e82e509284a56961d6e7895ad0d56
EdsonSMartins <smartins.edson@gmail.com>
2024-02-14 22:47:02 +0000

    Argument error: metadata class issue

--- sql-expression.py
+++ sql-expression.py
@@ -9,31 +9,31 @@ meta = MetaData(db)
 
 # create variable for "Artist" table
 artist_table = Table(
-    "Artist", meta,
-    Column("ArtistId", Integer, primary_key=True),
-    Column("Name", String)
+    "artist", meta,
+    Column("artistid", Integer, primary_key=True),
+    Column("name", String)
 )
 
 # create variable for "Album" table
 album_table = Table(
-    "Album", meta,
-    Column("AlbumId", Integer, primary_key=True),
-    Column("Title", String),
-    Column("ArtistId", Integer, ForeignKey("artist_table.ArtistId"))
+    "album", meta,
+    Column("albumid", Integer, primary_key=True),
+    Column("title", String),
+    Column("artistid", Integer, ForeignKey("artist_table.artistid"))
 )
 
 # create variable for "Track" table
 track_table = Table(
-    "Track", meta,
-    Column("TrackId", Integer, primary_key=True),
-    Column("Name", String),
-    Column("AlbumId", Integer, ForeignKey("album_table.AlbumId")),
-    Column("MediaTypeId", Integer, primary_key=False),
-    Column("GenreId", Integer, primary_key=False),
-    Column("Composer", String),
-    Column("Milliseconds", Integer),
-    Column("Bytes", Integer),
-    Column("UnitPrice", Float)
+    "track", meta,
+    Column("trackid", Integer, primary_key=True),
+    Column("name", String),
+    Column("albumid", Integer, ForeignKey("album_table.albumid")),
+    Column("mediatypeid", Integer, primary_key=False),
+    Column("genreid", Integer, primary_key=False),
+    Column("composer", String),
+    Column("milliseconds", Integer),
+    Column("bytes", Integer),
+    Column("unitprice", Float)
 )
 
 # making the connection
@@ -43,19 +43,19 @@ with db.connect() as connection:
     # select_query = artist_table.select()
 
     # Query 2 - select only the "Name" column from the "Artist" table
-    # select_query = artist_table.select().with_only_columns([artist_table.c.Name])
+    # select_query = artist_table.select().with_only_columns([artist_table.c.name])
 
     # Query 3 - select only 'Queen' from the "Artist" table
-    # select_query = artist_table.select().where(artist_table.c.Name == "Queen")
+    select_query = artist_table.select().where(artist_table.c.name == "Queen")
 
     # Query 4 - select only by 'ArtistId' #51 from the "Artist" table
-    # select_query = artist_table.select().where(artist_table.c.ArtistId == 51)
+    # select_query = artist_table.select().where(artist_table.c.artistid == 51)
 
     # Query 5 - select only the albums with 'ArtistId' #51 on the "Album" table
-    # select_query = album_table.select().where(album_table.c.ArtistId == 51)
+    # select_query = album_table.select().where(album_table.c.artistid == 51)
 
     # Query 6 - select all tracks where the composer is 'Queen' from the "Track" table
-    select_query = track_table.select().where(track_table.c.Composer == "Queen")
+    # select_query = track_table.select().where(track_table.c.composer == "Queen")
 
     results = connection.execute(select_query)
     for result in results:
